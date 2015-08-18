787387efff7372f8a71d4827b4e879951405a1f3
Luciano da Silva <luciano.silva@live.com.pt>
2015-08-18 18:03:32 +0100

    Simplify debug break

--- proto/include/proto.debug_break.h
+++ proto/include/proto.debug_break.h
@@ -1,4 +1,4 @@
-
+#pragma once
 #ifndef _proto_debug_break_h_
 #define _proto_debug_break_h_
 
@@ -12,64 +12,13 @@
 
 #ifdef PROTO_COMPILER_MSVC
 
-#define proto_debug_break __debugbreak
+#define proto_debug_break() __debugbreak;
 
 #else
 
-#include <signal.h>
-#include <unistd.h>
-#include <sys/syscall.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-	
-#	define DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP 0
-	
-#if defined(__i386__) || defined(__x86_64__)
-	
-#	define HAVE_TRAP_INSTRUCTION 1
-#	define trap_instruction __asm__ volatile("int $0x03")
-	
-#elif defined(__thumb__)
-	
-#	define HAVE_TRAP_INSTRUCTION 1
-	
-#	if 1
-#		define trap_instruction __asm__ volatile(".inst 0xde01")
-#	else
-#		define trap_instruction __asm__ volatile(".inst.w 0xf7f0a000")
-#	endif
-	
-#elif defined(__arm__) && !defined(__thumb__)
-	
-#	define HAVE_TRAP_INSTRUCTION 1
-#	define trap_instruction __asm__ volatile(".inst 0xe7f001f0")
-	
-#else
-	
-#	define HAVE_TRAP_INSTRUCTION 0
-	
-#endif
-	
-#if HAVE_TRAP_INSTRUCTION
-#	define proto_debug_break() trap_instruction
-#else
-#	if DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP
-#		define proto_debug_break() __builtin_trap ()
-#	else
-#		define proto_debug_break() raise(SIGTRAP)
-#	endif
-#endif
-	
-#ifdef __cplusplus
-}
-#endif
+#define proto_debug_break() __asm__ volatile("int $0x03")
 
 #endif
-
-#undef DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP
-#undef HAVE_TRAP_INSTRUCTION
-
 #endif
+
 #endif
--- proto/src/proto.debug.cpp
+++
@@ -1,1 +0,0 @@
-#include "proto.debug.h"
