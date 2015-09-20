b27e02cca812d26385dba4c7f56f81d472afc920
Luciano da Silva <luciano.silva@live.com.pt>
2015-09-20 17:48:49 +0100

    Starting of TUI module
    Starting simple a Terminal User Interface module

--- proto/modules/demo/src/main.cpp
+++ proto/modules/demo/src/main.cpp
@@ -1,5 +1,6 @@
 
 #include <proto.h>
+#include <proto.tui.h>
 
 int main(int arg_c, char * arg_v[]) {
 	return 0;
---
+++ proto/modules/proto_tui/include/proto.tui.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#ifndef	_proto_tui_h_
+#define _proto_tui_h_
+
+#endif
